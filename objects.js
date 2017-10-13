0e67c3a0582aa7c85bb4a306b57c13c3c4f95a15
Kevin M <shoron@gmail.com>
2017-10-13 05:34:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function updatePlaylist (obj, key, value) {
 		   obj[key] = value
 		   return obj
    }
-function removeFromPlaylist (obj, key, value) {
+function removeFromPlaylist (obj, key) {
     delete.obj[key]
     return obj
 }
